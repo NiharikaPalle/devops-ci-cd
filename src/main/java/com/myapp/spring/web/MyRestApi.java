2857d10735c76939c35458a4e49ec57697516cdb
NiharikaPalle <palleniharikajsn@gmail.com>
2022-02-01 15:46:05 +0530

    new project

---
+++ src/main/java/com/myapp/spring/web/MyRestApi.java
@@ -0,0 +1,21 @@
+package com.myapp.spring.web;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MyRestApi {
+	
+	@GetMapping
+	public String sayHello() {
+		
+		return "Welcome To World Of Rest Api Java Spring and Current Date "
+		+LocalDate.now();
+	}
+	
+
+}
